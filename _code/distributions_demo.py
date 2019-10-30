590837cb48e9805b18fc6c4bb0ecda12c408d600
Joe Cipolla <joecipolla@Joes-MacBook-Pro.local>
2019-10-30 16:49:29 -0500

    skewed data creation in python script

--- _code/distributions_demo.py
+++ _code/distributions_demo.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+from scipy.stats import skewnorm
 
 sns.set()
 
@@ -18,8 +19,8 @@ n = 10000  # number of samples
 normal_values = np.random.normal(mu, sigma, n)
 
 # plot histogram
-_, bins, __ = plt.hist(normal_values, 50, density=True)
-plt.title(f'Histogram of {n} random Normal Distribution samples')
+_, bins, __ = plt.hist(normal_values, 20, density=True, alpha=0.8)
+plt.title(f'Histogram of {n} Normal Distribution')
 plt.ylabel('Probability Density')
 plt.xlabel('Standard Deviation')
 
@@ -28,4 +29,36 @@ plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
          np.exp(-(bins - mu) ** 2 / (2 * sigma ** 2)),
          linewidth=2, color='r')
 plt.savefig(root_dir + '/images/normal_distribution.png')
+plt.show()
+
+
+# create right skewed distribution
+skew_factor = 4
+rv = skewnorm(skew_factor)
+x = np.linspace(skewnorm.ppf(0.000000001, skew_factor), skewnorm.ppf(0.9999999999, skew_factor), 100)
+x -= 10
+plt.plot(x, rv.pdf(x), 'k-', lw=2, color='r')
+right_skewed = skewnorm.rvs(skew_factor, size=10000)
+right_skewed -= 10
+plt.hist(right_skewed, 20, density=True, alpha=0.8)
+plt.title(f'Histogram of {n} Right Skewed Normal Distribution')
+plt.ylabel('Probability Density')
+plt.xlabel('Standard Deviation')
+# plt.savefig(...)
+plt.show()
+
+
+# create left skewed distribution
+skew_factor = -4
+rv = skewnorm(skew_factor)
+x = np.linspace(skewnorm.ppf(0.000000001, skew_factor), skewnorm.ppf(0.9999999999, skew_factor), 100)
+x += 10
+plt.plot(x, rv.pdf(x), 'k-', lw=2, color='r')
+left_skewed = skewnorm.rvs(skew_factor, size=10000)
+left_skewed += 10
+plt.hist(left_skewed, 20, density=True, alpha=0.8)
+plt.title(f'Histogram of {n} Left Skewed Normal Distribution')
+plt.ylabel('Probability Density')
+plt.xlabel('Standard Deviation')
+# plt.savefig(...)
 plt.show()
